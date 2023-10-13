8c7176acc998d421f4e9cdf4d031b349cb728d26
Bit <jumiciobi@gmail.com>
2023-10-13 07:49:13 +0100

    Added Game
    In Game added Player Controller with sprint, walk and jump functionality.

---
+++ Assets/Code/GroundCheck.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+    private PlayerController player;
+
+    private void Start() {
+        player = GetComponentInParent<PlayerController>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Ground")) player.isGrounded = true;
+    }
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Ground")) player.isGrounded = false;
+    }
+}

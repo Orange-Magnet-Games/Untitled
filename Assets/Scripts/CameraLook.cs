2ad38103b87dfa35012a92a20796c04856f79188
BitFl1p <jumiciobi@gmail.com>
2024-01-04 17:41:44 +0000

    restart work on project

--- Assets/Scripts/CameraLook.cs
+++ Assets/Scripts/CameraLook.cs
@@ -8,24 +8,25 @@ public class CameraLook : MonoBehaviour
 {
     private PlayerController player;
     private Vector2 input;
+    [SerializeField, Range(0, 1)] private float sensitivity = 1.0f;
     private void Start()
     {
         player = Manager.instance.player;
-        player.input.Camera.Look.performed += ctx => OnDirection(ctx, true);
-        player.input.Camera.Look.canceled += ctx => OnDirection(ctx, false);
+        player.input.Camera.Look.performed += ctx => OnLook(ctx, true);
+        player.input.Camera.Look.canceled += ctx => OnLook(ctx, false);
 
     }
 
-    private void OnDirection(InputAction.CallbackContext ctx, bool performed)
+    private void OnLook(InputAction.CallbackContext ctx, bool performed)
     {
         input = performed ? ctx.ReadValue<Vector2>() : Vector2.zero;
     }
     void Update()
     {
-        player.transform.eulerAngles += new Vector3(0, input.x, 0);
+        player.transform.eulerAngles += new Vector3(0, input.x * sensitivity, 0);
         var eulerAngles = transform.eulerAngles;
         
-        eulerAngles -= new Vector3(input.y, 0, 0);
+        eulerAngles -= new Vector3(input.y * sensitivity, 0, 0);
         eulerAngles = new Vector3(ClampAngle(eulerAngles.x, -90, 90), eulerAngles.y, eulerAngles.z);
         
         transform.eulerAngles = eulerAngles;
